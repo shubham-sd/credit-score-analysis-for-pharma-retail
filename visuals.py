b93d4d271f6e46ceda3df0ebe53e489866be32d8
shubham-sd <shubhamdandekar20@gmail.com>
2022-08-17 15:24:02 +0530

    chaned visuals.py file

--- visuals.py
+++ visuals.py
@@ -7,13 +7,13 @@ import seaborn as sns
 
 
 def vis_1():
-    st.write('### Credit Analysis of Retailers Segmentation')
-    filename = 'rfm_dataset2.sav'
-    df = pickle.load(open(filename, 'rb'))
+    st.write("### Credit Analysis of Retailers Segmentation")
+    filename = "rfm_dataset2.sav"
+    df = pickle.load(open(filename, "rb"))
 
-    f = open('./j_files/retailersnames.json')
+    f = open("./j_files/retailersnames.json")
     list1 = json.load(f)
-    f = open('./j_files/retailersmoney.json')
+    f = open("./j_files/retailersmoney.json")
     x = json.load(f)
 
     name = list1[0:10]
@@ -22,22 +22,21 @@ def vis_1():
     fig = plt.figure(figsize=(10, 5))
 
     # creating the bar plot
-    st.write('##### Graph for Monetary of Customer')
-    plt.bar(name, price, color='red',
-            width=0.2)
+    st.write("##### Graph for Monetary of Customer")
+    plt.bar(name, price, color="red", width=0.2)
     plt.xticks(rotation=40)
     plt.xlabel("x")
     plt.ylabel("y")
     plt.title("top retailers according to monetary")
     plt.show()
-    #fig1,ax1= plt.subplots()
+    # fig1,ax1= plt.subplots()
 
     # ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
     st.pyplot(fig)
     # ______________________________________________________________________________________________________________monetary
-    f = open('./j_files/retailersnames1.json')
+    f = open("./j_files/retailersnames1.json")
     list1 = json.load(f)
-    f = open('./j_files/retailersfrequency.json')
+    f = open("./j_files/retailersfrequency.json")
     x = json.load(f)
 
     name = list1[0:20]
@@ -46,9 +45,8 @@ def vis_1():
     fig = plt.figure(figsize=(10, 5))
 
     # creating the bar plot
-    st.write('##### Graph for Frequency of Customer')
-    plt.bar(name, price, color='cyan',
-            width=0.2)
+    st.write("##### Graph for Frequency of Customer")
+    plt.bar(name, price, color="cyan", width=0.2)
     plt.xticks(rotation=40)
     plt.xlabel("x")
     plt.ylabel("y")
@@ -56,30 +54,36 @@ def vis_1():
     plt.show()
     st.pyplot(fig)
     # _____________________________________________________________________________________________________________________
-    st.write('#### Recency of Retailers')
-    df2 = df.sort_values(by=['recency'], ascending=False)
+    st.write("#### RFM Score for Retailers")
+    # st.text("(for example RetailerID2)")
+    df2 = df.sort_values(by=["recency"], ascending=False)
+    df2 = df2["retailer_names"]
     st.dataframe(data=df2, width=None, height=None)
     x = st.text_input("Enter the retailer name", "")
-    if st.button('analyse'):
+    if st.button("analyse"):
         list2 = []
         tuple1 = tuple()
         for j in range(df.shape[0]):
-            if df['retailer_names'][j] == x:
-                tuple1 = (x, df['recency_rank'][j],
-                          df['frequency_rank'][j], df['monetary_rank'][j])
+            if df["retailer_names"][j] == x:
+                tuple1 = (
+                    x,
+                    df["recency_rank"][j],
+                    df["frequency_rank"][j],
+                    df["monetary_rank"][j],
+                )
                 break
 
         fig = plt.figure()
 
-        langs = ['Rencency score', 'Frequency_score', 'Monetary_score']
+        langs = ["Rencency score", "Frequency_score", "Monetary_score"]
         students = [round(tuple1[1]), round(tuple1[2]), round(tuple1[3])]
         sns.barplot(langs, students)
         xlocs = [i for i in range(0, 3)]
-        plt.xticks(rotation='vertical')
+        plt.xticks(rotation=40)
         for i, v in enumerate(students):
             plt.text(xlocs[i] - 0.25, v + 0.01, str(v))
         plt.xlabel("x")
         plt.ylabel("y")
-        plt.title("RFM Score of "+str(tuple1[0]))
+        plt.title("RFM Score of " + str(tuple1[0]))
         plt.show()
         st.pyplot(fig)
